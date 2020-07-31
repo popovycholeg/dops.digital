fdbd9870e72e4270ff5053829e20a6edd3380ecb
Oleg Popovych <magmoba@gmail.com>
2020-07-31 22:58:34 +0300

    Footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,18 +1,94 @@
 import React from "react";
+import styles from "./styles.module.css";
 
 const Footer = () => {
   return (
-    <footer>
-        {/* <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <footer className={styles.container}>
+      <div className={styles.logoContainer}>
+        <p className={styles.logo}>Logo</p>
+        <p className={styles.toTop}>to top</p>
+      </div>
+      <div className={styles.horizontalLine}></div>
+      <div className={styles.linksContainer}>
+        <div className={styles.contentContainer}>
+          <p className={`medium-text`}>Navigation</p>
+          <div className={styles.links}>
+            <a href='#' className={`small-text ${styles.link}`}>
+              Works
+            </a>
+            <a href='#' className={`small-text ${styles.link}`}>
+              About us
+            </a>
+            <a href='#' className={`small-text ${styles.link}`}>
+              Services
+            </a>
+            <a href='#' className={`small-text ${styles.link}`}>
+              Contacts
+            </a>
+          </div>
+        </div>
+        <div className={styles.contentContainer}>
+          <p className={`medium-text`}>Social Media</p>
+          <div className={styles.links}>
+            <a href='#' className={`small-text ${styles.link}`}>
+              Instagram
+            </a>
+            <a href='#' className={`small-text ${styles.link}`}>
+              LinkedIn
+            </a>
+            <a href='#' className={`small-text ${styles.link}`}>
+              Facebook
+            </a>
+            <a href='#' className={`small-text ${styles.link}`}>
+              Behance
+            </a>
+          </div>
+        </div>
+        <div className={styles.contentContainer}>
+          <p className={`medium-text`}>Contact us</p>
+          <div className={styles.links}>
+            <p className={`small-text ${styles.link}`}>
+              55 New Montgomery, Suite 607
+            </p>
+            <p className={`small-text ${styles.link}`}>
+              San Francisco, CA 94105
+            </p>
+            <p className={`small-text ${styles.link}`}>(800) 566-2340</p>
+          </div>
+        </div>
+      </div>
+      <div className={`${styles.horizontalLine} ${styles.marginBottom}`}></div>
+      <div className={styles.logoContainer}>
+        <a
+          href='#'
+          className={`small-text ${styles.link} ${styles.marginLeft}`}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a> */}
-      </footer>
+          Privacy Policy
+        </a>
+        <a
+          href='#'
+          className={`small-text ${styles.link} ${styles.marginLeft}`}
+        >
+          Terms of Use
+        </a>
+        <a
+          href='#'
+          className={`small-text ${styles.link} ${styles.alignCenter}`}
+        >
+          Copyright © 2019
+        </a>
+        <div className={styles.dopsDigital}>
+          <p className={`small-text ${styles.link}`}>created by  </p>
+          <a
+            href='#'
+            className={`small-text ${styles.dopsDigital} ${styles.marginLeft}`}
+          >
+            dops.digital
+          </a>
+        </div>
+      </div>
+    </footer>
   );
-}
+};
 
 export default Footer;
