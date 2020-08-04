4315b35db5a3faedd415a40b1d9e0f03922a20c6
Oleg Popovych <magmoba@gmail.com>
2020-08-04 17:21:34 +0300

    Add Words

--- components/CommentSection/CommentSection.jsx
+++ components/CommentSection/CommentSection.jsx
@@ -1,20 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import styles from "./styles.module.css";
 
 const CommentSection = () => {
   const [lettersCount, setLettersCount] = useState(0);
   const handleChange = (event) => {
-    setLettersCount(event.target.value.split(' ').length);
-  }
+    setLettersCount(event.target.value.split(" ").length);
+  };
   return (
     <section className={styles.container}>
-      <p className={`small-text ${styles.label} ${styles.marginTop}`}>Leave comment</p>
-      <textarea className={styles.textArea} placeholder='Paste your text to count words...' onChange={handleChange}>
-      </textarea>
-      <div className={styles.wordsCounter}>{lettersCount}</div>
+      <p className={`small-text ${styles.label} ${styles.marginTop}`}>
+        Leave comment
+      </p>
+      <textarea
+        className={styles.textArea}
+        placeholder='Paste your text to count words...'
+        onChange={handleChange}
+      ></textarea>
+      <div className={styles.flexRow}>
+        <div className={styles.wordsCounter}>{lettersCount}</div>
+        <p className={`small-text ${styles.marginLeft}`}>Words</p>
+      </div>
     </section>
   );
 };
-
 export default CommentSection;
