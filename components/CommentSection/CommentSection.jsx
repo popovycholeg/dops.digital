d90e36bcd31d00cf16290a91cd2003a987b03f66
Oleg Popovych <magmoba@gmail.com>
2020-08-05 12:41:51 +0300

    Count words improve function

--- components/CommentSection/CommentSection.jsx
+++ components/CommentSection/CommentSection.jsx
@@ -1,17 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import styles from "./styles.module.css";
 
 const CommentSection = () => {
   const [lettersCount, setLettersCount] = useState(0);
+  const countWords = (s) => {
+    s = s.replace(/(^\s*)|(\s*$)/gi, "");
+    s = s.replace(/[ ]{2,}/gi, " ");
+    s = s.replace(/\n /, "\n");
+    return s.split(" ").filter((str) => str != "").length;
+  };
+
   const handleChange = (event) => {
-    setLettersCount(event.target.value.split(' ').length);
-  }
+    setLettersCount(countWords(event.target.value));
+  };
   return (
     <section className={styles.container}>
-      <p className={`small-text ${styles.label} ${styles.marginTop}`}>Leave comment</p>
-      <textarea className={styles.textArea} placeholder='Paste your text to count words...' onChange={handleChange}>
-      </textarea>
+      <p className={`small-text ${styles.label} ${styles.marginTop}`}>
+        Leave comment
+      </p>
+      <textarea
+        className={styles.textArea}
+        placeholder='Paste your text to count words...'
+        onChange={handleChange}
+      ></textarea>
       <div className={styles.wordsCounter}>{lettersCount}</div>
     </section>
   );
